351c7fb138e43b73cc12d4253a7ce9438d5d6556
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-07-17 00:29:21 +0300

    dating starter code

---
+++ app/src/main/java/kapadokia/nyandoro/dating/util/Resources.java
@@ -0,0 +1,9 @@
+package kapadokia.nyandoro.dating.util;
+
+
+import kapadokia.nyandoro.dating.R;
+
+public class Resources {
+
+    public static final int BACKGROUND_HEARTS = R.drawable.heart_background;
+}
