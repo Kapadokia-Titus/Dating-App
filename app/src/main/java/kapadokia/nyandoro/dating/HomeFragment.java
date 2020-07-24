e139c3bac1ed71d62033f0cbed7296dee0abe506
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-07-25 02:31:55 +0300

    loaded the UI

--- app/src/main/java/kapadokia/nyandoro/dating/HomeFragment.java
+++ app/src/main/java/kapadokia/nyandoro/dating/HomeFragment.java
@@ -3,21 +3,61 @@ package kapadokia.nyandoro.dating;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
+import kapadokia.nyandoro.dating.models.User;
+import kapadokia.nyandoro.dating.util.Users;
+
 public class HomeFragment extends Fragment {
 
     private static final String TAG = "HomeFragment";
+    private static final int NUM_COLUMNS = 2;
+
+    private RecyclerView recyclerView;
+
+    // vars
+    private ArrayList<User> mMatches = new ArrayList<>();
+    private StaggeredGridLayoutManager staggeredGridLayoutManager;
+    private MainRecyclerViewAdapter mainRecyclerViewAdapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home, container,false);
         Log.d(TAG, "onCreateView: started");
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        findMatches();
         return view;
+    }
+
+    private void findMatches(){
+        Users users  = new Users();
+        if (mMatches!=null){
+            mMatches.clear();
+        }
+        for (User user:users.USERS){
+            mMatches.add(user);
+        }
+        if (mainRecyclerViewAdapter==null){
+            initRecyclerview();
+        }
+    }
+
+    private void initRecyclerview() {
+        Log.d(TAG, "initRecyclerview: init recyclerview");
+        staggeredGridLayoutManager = new StaggeredGridLayoutManager(NUM_COLUMNS, LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
+        mainRecyclerViewAdapter = new MainRecyclerViewAdapter(getActivity(), mMatches);
+        recyclerView.setAdapter(mainRecyclerViewAdapter);
     }
 }
--- app/src/main/java/kapadokia/nyandoro/dating/util/Users.java
+++ app/src/main/java/kapadokia/nyandoro/dating/util/Users.java
@@ -17,71 +17,71 @@ public class Users {
     /*
             Men
      */
-    public static final User James = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.james).toString(),
+    public static final User James = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.james).toString(),
             "James", "Male","Female", "Looking");
 
-    public static final User Robert = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.robert).toString(),
+    public static final User Robert = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.robert).toString(),
             "Robert", "Male","Female", "Looking");
 
-    public static final User Michael = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.michael).toString(),
+    public static final User Michael = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.michael).toString(),
             "Michael", "Male","Female", "Looking");
 
-    public static final User William = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.william).toString(),
+    public static final User William = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.william).toString(),
             "William", "Male","Female", "Not Looking");
 
-    public static final User Joseph = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.joseph).toString(),
+    public static final User Joseph = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.joseph).toString(),
             "Joseph", "Male","Female", "Looking");
 
-    public static final User Charles = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.charles).toString(),
+    public static final User Charles = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.charles).toString(),
             "Charles", "Male","Female", "Looking");
 
-    public static final User Matthew = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.mattew).toString(),
+    public static final User Matthew = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.mattew).toString(),
             "Matthew", "Male","Female", "Looking");
 
-    public static final User Donald = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.donald).toString(),
+    public static final User Donald = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.donald).toString(),
             "Donald", "Male","Female", "Looking");
 
-    public static final User Paul = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.paul).toString(),
+    public static final User Paul = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.paul).toString(),
             "Paul", "Male","Female", "Looking");
 
-    public static final User Steve = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.steve).toString(),
+    public static final User Steve = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.steve).toString(),
             "Steve", "Male","Female", "Looking");
 
 
     /*
             Females
      */
-    public static final User Mary = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.mary).toString(),
+    public static final User Mary = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.mary).toString(),
             "Mary", "Female","Male", "Looking");
 
-    public static final User Patricia = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.patricia).toString(),
+    public static final User Patricia = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.patricia).toString(),
             "Patricia", "Female","Male", "Looking");
 
-    public static final User Jennifer = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.jennifer).toString(),
+    public static final User Jennifer = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.jennifer).toString(),
             "Jennifer", "Female","Male", "Looking");
 
-    public static final User Elizabeth = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.elizabeth).toString(),
+    public static final User Elizabeth = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.elizabeth).toString(),
             "Elizabeth", "Female","Male", "Looking");
 
-    public static final User Linda = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.linda).toString(),
+    public static final User Linda = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.linda).toString(),
             "Linda", "Female","Male", "Looking");
 
-    public static final User Susan = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.susan).toString(),
+    public static final User Susan = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.susan).toString(),
             "Susan", "Female","Male", "Looking");
 
-    public static final User Jessica = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.jessica).toString(),
+    public static final User Jessica = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.jessica).toString(),
             "Jessica", "Female","Male", "Looking");
 
-    public static final User Sarah = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.sarah).toString(),
+    public static final User Sarah = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.sarah).toString(),
             "Sarah", "Female","Male", "Looking");
 
-    public static final User Karen = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.karen).toString(),
+    public static final User Karen = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.karen).toString(),
             "Karen", "Female","Male", "Looking");
 
-    public static final User Nancy = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.nancy).toString(),
+    public static final User Nancy = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.nancy).toString(),
             "Nancy", "Female","Male", "Looking");
 
-    public static final User Carol = new User(Uri.parse("android.resource://codingwithmitch.com.tabiandating/" + R.drawable.carol).toString(),
+    public static final User Carol = new User(Uri.parse("android.resource://kapadokia.nyandoro.dating/" + R.drawable.carol).toString(),
             "Carol", "Do Not Identify","Anyone", "Looking");
 }
 
